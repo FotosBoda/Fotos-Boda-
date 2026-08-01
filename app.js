c41ca8f23d2d47a6bbedeeb795b27e60bdfb8fef
FotosBoda <cristinabelmonteespejo94@gmail.com>
2026-08-01 16:13:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://vizujiwztwdydalzjben.supabase.co";
 const SUPABASE_KEY = "sb_publishable_YDn4e0TJNmKGF1FugH5ROA_IYIvDeEP";
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const clienteSupabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 document.getElementById("subirFotos").addEventListener("click", () => {
     alert("Conectado con Supabase correctamente ✅");
